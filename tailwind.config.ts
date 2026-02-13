11708eb57927aa3bddd88e9c379aae666ca14e8f
HardyPie <hardypie0@gmail.com>
2026-02-13 16:53:39 +0000

    in landing page and login, sign up page add this animated background : B

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -104,7 +104,7 @@ export default {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'animated-gradient': 'animated-gradient 3s ease-in-out infinite',
-        'background-pan': 'background-pan 15s ease infinite',
+        'background-pan': 'background-pan 10s ease infinite',
       },
     },
   },
