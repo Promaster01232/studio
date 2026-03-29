ebd427be2e668dfc8683868d21f80f71c8723b42
Enter Space <enterspaceindia@gmail.com>
2026-03-29 01:08:55 +0000

    <!-- Google tag (gtag.js) --> <script async src="https://www.googletagma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -106,6 +106,22 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-R8G6PW6X1K"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-R8G6PW6X1K');
+          `}
+        </Script>
+        
         <Script
           id="structured-data"
           type="application/ld+json"
